9ab961a35d1c78f836260cc6c79403ec37203118
Nuttapong <thenuts53@gmail.com>
2025-07-24 17:10:02 +0700

    - Alignment margin and padding - map lists

--- lib/question_screen.dart
+++ lib/question_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_project2/answer_button.dart';
+import 'package:flutter_project2/data/question.dart';
 
 class QuestionScreen extends StatefulWidget {
   const QuestionScreen({super.key});
@@ -12,21 +14,26 @@ class QuestionScreen extends StatefulWidget {
 class _QuistionScreenState extends State<QuestionScreen> {
   @override
   Widget build(BuildContext context) {
+    final currentQuestion = questions[0];
     return SizedBox(
       width: double.infinity,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text('The Question...'),
-          SizedBox(height: 30),
-          ElevatedButton(onPressed: () {}, child: Text('Answers 1')),
-          SizedBox(height: 30),
-          ElevatedButton(onPressed: () {}, child: Text('Answers 2')),
-          SizedBox(height: 30),
-          ElevatedButton(onPressed: () {}, child: Text('Answers 3')),
-          SizedBox(height: 30),
-          ElevatedButton(onPressed: () {}, child: Text('Answers 4')),
-        ],
+      child: Container(
+        margin: const EdgeInsets.all(40),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text(
+              currentQuestion.text,
+              style: const TextStyle(color: Colors.white),
+              textAlign: TextAlign.center,
+            ),
+            SizedBox(height: 30),
+            ...currentQuestion.answers.map((item) {
+              return AnswerButton(answerText: item, onTap: () {});
+            }),
+          ],
+        ),
       ),
     );
   }
